52f291d47728a65a92d0fd7914ff380ac74517fb
Marvan <marvansm@code.edu.az>
2025-10-15 20:19:42 +0400

    feat: added blog detail page

--- client/src/Featured/Common/Heading/index.jsx
+++ client/src/Featured/Common/Heading/index.jsx
@@ -4,7 +4,7 @@ const HeadingText = ({ title, desc }) => {
   return (
     <div className=" mx-auto max-w-[1320px] px-[10px] my-[200px]">
       <div className="flex items-center justify-between">
-        <div className="w-[300px] border-t border-gray-200"></div>
+        <div className="w-[350px] border-t border-gray-200"></div>
         <div className="flex items-center flex-col pt-[40px]">
           <h2 className="text-[36px] leading-[36px] font-normal text-[#201f1f] mb-[10px]">
             {title}
@@ -13,7 +13,7 @@ const HeadingText = ({ title, desc }) => {
             {desc}
           </p>
         </div>
-        <div className="w-[300px] border-t border-gray-200"></div>
+        <div className="w-[350px] border-t border-gray-200"></div>
       </div>
     </div>
   );
--- client/src/Featured/Components/TrendsSection/index.jsx
+++ client/src/Featured/Components/TrendsSection/index.jsx
@@ -1,10 +1,14 @@
-
 import HeadingText from "../../Common/Heading";
 
 const TrendsSection = () => {
   return (
     <div>
-      <HeadingText />
+      <HeadingText
+        title={"Trending Products"}
+        desc={
+          "Mirum est notare quam littera gothica quam nunc putamus parum claram!"
+        }
+      />
     </div>
   );
 };
